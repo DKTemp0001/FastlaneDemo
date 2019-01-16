f71dc026f899666e57f446d59d75d3331aa53c5a
DhaivatNimble <dhaivat@nimblechapps.com>
2019-01-16 09:01:04 +0530

    Initialised iOS Fastlane Demo

---
+++ FastlaneDemo/FastlaneDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FastlaneDemo
+//
+//  Created by Nimblechapps iOS on 16/01/19.
+//  Copyright © 2019 Nimblechapps iOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
